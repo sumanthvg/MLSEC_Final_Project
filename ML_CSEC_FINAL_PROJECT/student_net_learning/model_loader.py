432a71bd56e7cbba9c6c3b512adf3908465d785b
sumanthvg <42473743+sumanthvg@users.noreply.github.com>
2022-12-20 13:27:05 -0500

    Update model_loader.py

--- ML_CSEC_FINAL_PROJECT/student_net_learning/model_loader.py
+++ ML_CSEC_FINAL_PROJECT/student_net_learning/model_loader.py
@@ -1,7 +1,7 @@
 
 
 def get_model_net(model_name):
-    from student_net_learning.models.densenet import densenet201
-    print('Loading DenseNet121')
-    net = densenet201(pretrained=True)
+    from student_net_learning.models.vgg19 import vgg19
+    print('Loading VGG 19')
+    net = vgg19(pretrained=True)
     return net
