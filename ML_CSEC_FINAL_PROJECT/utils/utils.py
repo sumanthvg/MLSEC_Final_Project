171875075da90a498d0febb13431f99c9cccce4a
sumanthvg <42473743+sumanthvg@users.noreply.github.com>
2022-12-23 04:25:10 -0500

    Update utils.py

--- ML_CSEC_FINAL_PROJECT/utils/utils.py
+++ ML_CSEC_FINAL_PROJECT/utils/utils.py
@@ -1,8 +1,57 @@
 from skimage.measure import compare_ssim
 import numpy as np
+import torch
+from torch.optim import optimizer
 
 def get_ssim(original_img,changed_img):
     ssim = compare_ssim(np.array(original_img, dtype=np.float32),
                         np.array(changed_img, dtype=np.float32),
                         multichannel=True)
     return ssim
+
+class ADAMOptimizer(Optimizer):
+    """
+    implements ADAM Algorithm, as a preceding step.
+    """
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.99), eps=1e-8, weight_decay=0):
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+        super(ADAMOptimizer, self).__init__(params, defaults)
+        
+    def step(self):
+        loss = None
+        for group in self.param_groups:
+            for p in group['params']:
+                grad = p.grad.data
+                state = self.state[p]
+
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['exp_avg'] = torch.zeros_like(p.data)
+                    state['exp_avg_sq'] = torch.zeros_like(p.data)
+                    
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+
+                b1, b2 = group['betas']
+                state['step'] += 1
+
+                if group['weight_decay'] != 0:
+                    grad = grad.add(group['weight_decay'], p.data)
+
+                exp_avg = torch.mul(exp_avg, b1) + (1 - b1)*grad
+                exp_avg_sq = torch.mul(exp_avg_sq, b2) + (1-b2)*(grad*grad)
+                
+                suplicant = exp_avg_sq.sqrt() + group['eps']
+
+                bias_correction1 = 1 / (1 - b1 ** state['step'])
+                bias_correction2 = 1 / (1 - b2 ** state['step'])
+                
+                adapted_learning_rate = group['lr'] * bias_correction1 / math.sqrt(bias_correction2)
+
+                p.data = p.data - adapted_learning_rate * exp_avg / suplicant 
+                
+                if state['step']  % 10000 ==0:
+                    print ("group:", group)
+                    print("p: ",p)
+                    print("p.data: ", p.data)
+                
+        return loss
